f52412cbf93bcf7bc6ffdcd431eba1c394b1fee4
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-01-22 22:43:28 -0300

    Ajustes na Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { RouteComponentProps } from "react-router";
 import { FiUser } from "react-icons/fi";
 import { Link } from 'react-router-dom';
+import { logout } from '../services/auth';
+class Header extends React.Component<RouteComponentProps> {
+  handleLogout = () => {
+    const { history } = this.props;
+    
+    if(history) history.push('/');
+    logout();
+  }
 
-export default class Header extends React.Component {
-  render () {
+  render() {
     return (
       <header>
         <div className="header-title">
           <Link to="/">Gestor de ACCs</Link>
         </div>
 
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <div style={{marginRight: 10}}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ marginRight: 10, fontSize: '1rem' }}>
             Olá!
           </div>
-          <div className="header-avatar">
-            <FiUser />
-          </div>
+          <button onClick={this.handleLogout} style={{
+            border: 'none',
+            background: 'transparent',
+            margin: 0,
+            padding: 0,
+          }}>
+            <div className="header-avatar">
+              <FiUser />
+            </div>
+          </button>
         </div>
       </header>
     );
   }
 }
+
+export default withRouter(Header);
