b47fedf91d04b4a00f146e2435ab6d599c0cc21b
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-02-21 13:12:01 -0300

    Criação da Tela de Cadastro do Tipo de ACC

---
+++ src/components/Notifications.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Box, createStandaloneToast } from '@chakra-ui/react';
+import Noty from 'noty';
+
+const NOTY_THEME = 'nest';
+const toast = createStandaloneToast();
+
+const notifySuccess = (text: string): any => {
+  toast({
+    title: 'Sucesso!',
+    status: 'success',
+    duration: 3000,
+    isClosable: true,
+    position: 'bottom-right',
+    render: props => (
+      <Box m={3} color="white" p={3} bg="teal.500" borderRadius="md">
+        <p>
+          <strong>Sucesso!</strong>
+        </p>
+        <p>{text}</p>
+      </Box>
+    ),
+  });
+};
+
+function notifyError(text: string): void {
+  toast({
+    title: 'Sucesso!',
+    status: 'success',
+    duration: 3000,
+    isClosable: true,
+    position: 'bottom-right',
+    render: props => (
+      <Box m={3} color="white" p={3} bg="red.500" borderRadius="md">
+        <p>
+          <strong>Ops!</strong>
+        </p>
+        <p>{text}</p>
+      </Box>
+    ),
+  });
+}
+
+function notifyWarning(text: string): void {
+  new Noty({
+    text,
+    type: 'warning',
+    theme: NOTY_THEME,
+    timeout: 2000,
+    progressBar: true,
+  }).show();
+}
+
+export { notifySuccess, notifyError, notifyWarning };
