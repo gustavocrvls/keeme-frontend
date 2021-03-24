bf77e4a9963731d4084e1b2a2eab892763b8b116
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-23 21:16:40 -0300

    Refatoração do componente de detalhes da acc

--- src/views/discente/pages/DetalhesDaACC/index.tsx
+++ src/views/discente/pages/DetalhesDaACC/index.tsx
@@ -1,19 +1,22 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable camelcase */
 import React, { useEffect, useRef, useState } from 'react';
-import { useParams, withRouter } from 'react-router-dom';
-import { FiCircle, FiDownload, FiXCircle, FiCheckCircle } from 'react-icons/fi';
+import { useHistory, useParams, withRouter } from 'react-router-dom';
+import { FiDownload, FiEdit, FiTrash } from 'react-icons/fi';
 
-import { Box, Button, Stack } from '@chakra-ui/react';
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, Flex, IconButton, SimpleGrid, Stack, Tooltip } from '@chakra-ui/react';
 import STATUS_DA_ACC from '../../../../constants/StatusDaACC';
 import api from '../../../../services/api';
 import PageTitle from '../../../../components/PageTitle';
+import { notifyError, notifySuccess } from '../../../../components/Notifications';
 
 interface IAcc {
   id: number;
   id_certificado: number;
   pontos: number;
   quantidade: number;
-  sobre: string;
+  descricao: string;
+  criado_em: Date;
   status_da_acc: {
     id: number;
     nome: string;
@@ -26,13 +29,21 @@ interface IAcc {
       nome: string;
     };
   };
+  avaliacao_da_acc: {
+    id: number;
+    criado_em: Date;
+    descricao: string;
+    usuario: {
+      id: number;
+      nome: string;
+    };
+  };
+  vriante_de_acc: {
+    id: number,
+    descricao: string,
+    pontos_por_unidade: number
+  };
 }
-
-interface IStatus {
-  id: number;
-  nome: string;
-}
-
 interface ParamTypes {
   id: string;
 }
@@ -40,62 +51,77 @@ interface ParamTypes {
 export function DetalhesDaAcc(): JSX.Element {
   const downloadRef = useRef<HTMLAnchorElement>(null);
   const [acc, setACC] = useState<IAcc>();
+  const [pontos, setPontos] = useState<number>(0);
+  const [isAlertDeleteOpen, setIsAlertDeleteOpen] = useState<boolean>(false);
+  const cancelRef = useRef<any>();
+
+  const history = useHistory();
 
   const { id } = useParams<ParamTypes>();
 
   useEffect(() => {
     async function loadACC() {
-      const response = await api.get(`accs/${id}`);
-      setACC(response.data);
+      try {
+        const response = await api.get(`accs/${id}`);
+        setACC(response.data);
+
+        setPontos(response.data.quantidade * response.data.variante_de_acc.pontos_por_unidade)
+
+      } catch (err) {
+        notifyError('Não foi possível carregar os dados :(')
+      }
     }
     loadACC();
   }, []);
 
-  function handleStatusColor(status?: IStatus): JSX.Element {
-    let color = '';
-    let Icon = <></>;
-    if (status)
-      switch (status.id) {
-        case STATUS_DA_ACC.EM_ANALISE:
-          color = '#8FA7B2';
-          Icon = <FiCircle style={{ marginRight: 5 }} />;
-          break;
-        case STATUS_DA_ACC.APROVADA:
-          color = '#31878C';
-          Icon = <FiCheckCircle style={{ marginRight: 5 }} />;
-          break;
-        case STATUS_DA_ACC.NEGADA:
-          color = '#DE4079';
-          Icon = <FiXCircle style={{ marginRight: 5 }} />;
-          break;
+  function handleStatus() {
+    switch (acc?.status_da_acc.id) {
+      case STATUS_DA_ACC.APROVADA:
+        return (
+          <strong style={{ color: 'teal' }}>{acc.status_da_acc.nome}</strong>
+        );
 
-        default:
-          break;
-      }
+      case STATUS_DA_ACC.EM_ANALISE:
+        return (
+          <strong style={{ color: 'gray' }}>{acc.status_da_acc.nome}</strong>
+        );
 
-    return (
-      <span
-        style={{
-          fontSize: 12,
-          backgroundColor: color,
-          padding: '2px 5px',
-          color: '#fff',
-          borderRadius: 3,
-          display: 'flex',
-          alignItems: 'center',
-          width: 120,
-          justifyContent: 'center',
-        }}
-      >
-        {Icon}
-        {status?.nome}
-      </span>
-    );
+      case STATUS_DA_ACC.NEGADA:
+        return (
+          <strong style={{ color: 'tomato' }}>{acc.status_da_acc.nome}</strong>
+        );
+      default:
+        return <></>;
+    }
+  }
+
+  async function deleteACC() {
+    try {
+      await api.delete(`accs/${id}`);
+      notifySuccess('A ACC foi excluída')
+      history.goBack();
+    } catch (err) {
+      notifyError('Não foi possível excluir a ACC :(');
+    }
   }
 
   return (
     <>
-      <PageTitle backTo="/discente/minhas-accs">Detalhes da Acc</PageTitle>
+      <PageTitle
+        backTo="/discente/minhas-accs"
+        actions={(
+          <>
+            <Tooltip label="Editar" aria-label="Editar" hasArrow>
+              <IconButton aria-label="edit" icon={<FiEdit />} />
+            </Tooltip>
+            <Tooltip label="Excluir" aria-label="Excluir" hasArrow>
+              <IconButton colorScheme="red" aria-label="delete" icon={<FiTrash />} onClick={() => setIsAlertDeleteOpen(true)} />
+            </Tooltip>
+          </>
+        )}
+      >
+        Detalhes da ACC
+      </PageTitle>
 
       <Stack spacing="5" marginTop="5">
         <Box>
@@ -105,16 +131,27 @@ export function DetalhesDaAcc(): JSX.Element {
           <Box width="100%">{acc?.tipo_de_acc.nome}</Box>
         </Box>
 
+        {
+          acc?.criado_em && (
+          <Box>
+            <Box width="100%" color="gray.500">
+              Criada em
+            </Box>
+            <Box width="100%">{new Date(acc?.criado_em).toLocaleString()}</Box>
+          </Box>
+          )
+        }
+
         <Stack
           direction="row"
-          spacing={[0, 18, 20]}
+          spacing={[0, 100]}
           justifyContent={['space-between', 'flex-start']}
         >
           <Box>
             <Box width="100%" color="gray.500">
               Status
             </Box>
-            <Box width="100%">{handleStatusColor(acc?.status_da_acc)}</Box>
+            <Box width="100%">{handleStatus()}</Box>
           </Box>
 
           <Box>
@@ -126,9 +163,9 @@ export function DetalhesDaAcc(): JSX.Element {
 
           <Box>
             <Box width="100%" color="gray.500">
-              Pontuação
+              Pontos
             </Box>
-            <Box width="100%">{acc?.pontos}</Box>
+            <Box width="100%">{pontos}</Box>
           </Box>
         </Stack>
 
@@ -136,8 +173,35 @@ export function DetalhesDaAcc(): JSX.Element {
           <Box width="100%" color="gray.500">
             Descrição
           </Box>
-          <Box width="100%">{acc?.sobre}</Box>
+          <Box width="100%">{acc?.descricao}</Box>
         </Box>
+
+        {acc?.status_da_acc.id === STATUS_DA_ACC.APROVADA ||
+          (acc?.status_da_acc.id === STATUS_DA_ACC.NEGADA && (
+            <SimpleGrid columns={[1, 2]}>
+              <Box>
+                <Box width="100%" color="gray.500">
+                  Avaliada por
+                </Box>
+                <Box width="100%">{acc?.avaliacao_da_acc.usuario.nome}</Box>
+              </Box>
+              <Box>
+                <Box width="100%" color="gray.500">
+                  Data da Avaliação
+                </Box>
+                <Box width="100%">{new Date(acc?.avaliacao_da_acc.criado_em).toLocaleString()}</Box>
+              </Box>
+            </SimpleGrid>
+          ))}
+
+        {acc?.status_da_acc.id === STATUS_DA_ACC.NEGADA && (
+          <Box>
+            <Box width="100%" color="gray.500">
+              Motivo da Reprovação
+            </Box>
+            <Box width="100%">{acc?.avaliacao_da_acc.descricao}</Box>
+          </Box>
+        )}
 
         <Box display="flex" justifyContent="center">
           <Button
@@ -156,6 +220,47 @@ export function DetalhesDaAcc(): JSX.Element {
           </a>
         </Box>
       </Stack>
+
+
+      <AlertDialog
+        isOpen={isAlertDeleteOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={() => setIsAlertDeleteOpen(false)}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Excluir ACC
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              <p>
+                Tem certeza que deseja excluir a ACC?
+              </p>
+              {
+                acc?.status_da_acc.id === STATUS_DA_ACC.APROVADA &&
+                <p>A pontuação obtida nessa ACC será retirada da sua contagem total de pontos.</p>
+              }
+              <p>
+                Essa ação não pode ser desfeita.
+              </p>
+
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                onClick={() => setIsAlertDeleteOpen(false)}
+              >
+                Cancelar
+              </Button>
+              <Button colorScheme="red" onClick={deleteACC} ml={3}>
+                Confirmar
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </>
   );
 }
