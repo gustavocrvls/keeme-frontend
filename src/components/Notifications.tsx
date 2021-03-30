4d06a59bf314cd70a3cc741d2a55cdae7e8db286
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-30 08:31:52 -0300

    Remoção de bibliotecas e componentes inutilizados

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import { Box, createStandaloneToast } from '@chakra-ui/react';
-import Noty from 'noty';
 
-const NOTY_THEME = 'nest';
 const toast = createStandaloneToast();
 
-const notifySuccess = (text: string): any => {
+const notifySuccess = (text: string): void => {
   toast({
     title: 'Sucesso!',
     status: 'success',
     duration: 3000,
     isClosable: true,
     position: 'bottom-right',
-    render: props => (
+    render: () => (
       <Box m={3} color="white" p={3} bg="teal.500" borderRadius="md">
         <p>
           <strong>Sucesso!</strong>
@@ -30,7 +27,7 @@ function notifyError(text: string): void {
     duration: 3000,
     isClosable: true,
     position: 'bottom-right',
-    render: props => (
+    render: () => (
       <Box m={3} color="white" p={3} bg="red.500" borderRadius="md">
         <p>
           <strong>Ops!</strong>
@@ -48,7 +45,7 @@ function notifyWarning(text: string): void {
     duration: 3000,
     isClosable: true,
     position: 'bottom-right',
-    render: props => (
+    render: () => (
       <Box m={3} color="white" p={3} bg="yellow.500" borderRadius="md">
         <p>
           <strong>Ops!</strong>
