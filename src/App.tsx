72e390ae108927a76fc20fa219a47a22e76e66e8
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-24 13:38:49 -0300

    Remoção do tema no styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Routes from './routes';
 import { SidebarProvider } from './contexts/SidebarProvider';
-import { GlobalStyle } from './styles/global';
 
 import theme from './styles/theme';
 
 function App(): JSX.Element {
   return (
     <>
-      <GlobalStyle />
       <ChakraProvider theme={theme}>
         <div id="app-content">
           <SidebarProvider>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,6 +6,24 @@ const theme = extendTheme({
     heading: 'Noto Sans, system-ui, sans-serif',
     mono: 'Noto Sans, system-ui, sans-serif',
   },
+  styles: {
+    global: {
+      '*::-webkit-scrollbar-track': {
+        borderRadius: '10px',
+        backgroundColor: '#F5F5F5',
+      },
+
+      '*::-webkit-scrollbar': {
+        width: '8px',
+        backgroundColor: '#F5F5F5',
+      },
+
+      '*::-webkit-scrollbar-thumb': {
+        borderRadius: '10px',
+        backgroundColor: '#c9c9c9',
+      },
+    },
+  },
 });
 
 export default theme;
