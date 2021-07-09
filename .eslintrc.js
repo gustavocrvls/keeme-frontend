c17c39ea85e1d4d4a77e8bf4ba17b74c3467694d
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-07-09 16:49:29 -0300

    Correções na padronização do código

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
