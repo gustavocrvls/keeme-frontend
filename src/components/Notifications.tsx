211f125ad6f461b1199d8ef31b0389264a8abddf
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-07-09 17:09:02 -0300

    Refatoração das notificações

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,59 +1,35 @@
-import { Box, createStandaloneToast } from '@chakra-ui/react';
+import { createStandaloneToast } from '@chakra-ui/react';
 
 const toast = createStandaloneToast();
 
-const notifySuccess = (text: string): void => {
-  toast({
-    title: 'Sucesso!',
-    status: 'success',
-    duration: 3000,
-    isClosable: true,
-    position: 'bottom-right',
-    render: () => (
-      <Box m={3} color="white" p={3} bg="teal.500" borderRadius="md">
-        <p>
-          <strong>Sucesso!</strong>
-        </p>
-        <p>{text}</p>
-      </Box>
-    ),
-  });
-};
+const closeAfter = 1000 * 5; // 5 seconds
 
-function notifyError(text: string): void {
+export function notifySuccess(text: string): void {
   toast({
-    title: 'Sucesso!',
+    description: text,
     status: 'success',
-    duration: 3000,
+    duration: closeAfter,
     isClosable: true,
     position: 'bottom-right',
-    render: () => (
-      <Box m={3} color="white" p={3} bg="red.500" borderRadius="md">
-        <p>
-          <strong>Ops!</strong>
-        </p>
-        <p>{text}</p>
-      </Box>
-    ),
   });
 }
 
-function notifyWarning(text: string): void {
+export function notifyError(text: string): void {
   toast({
-    title: 'Sucesso!',
+    description: text,
+    status: 'error',
+    duration: closeAfter,
+    isClosable: true,
+    position: 'bottom-right',
+  });
+}
+
+export function notifyWarning(text: string): void {
+  toast({
+    description: text,
     status: 'warning',
-    duration: 3000,
+    duration: closeAfter,
     isClosable: true,
     position: 'bottom-right',
-    render: () => (
-      <Box m={3} color="white" p={3} bg="yellow.500" borderRadius="md">
-        <p>
-          <strong>Ops!</strong>
-        </p>
-        <p>{text}</p>
-      </Box>
-    ),
   });
 }
-
-export { notifySuccess, notifyError, notifyWarning };
