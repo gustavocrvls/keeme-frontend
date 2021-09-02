66c5e22c2095dd8f14b1a7237374b993689af2c6
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-09-02 09:56:15 -0300

    remoção do cpf

--- src/utils/masks.ts
+++
@@ -1,9 +0,0 @@
-export function cpfMask(cpf: string): string {
-  const maskedCPF = cpf
-    .replace(/\D/g, '')
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-    .replace(/(-\d{2})\d+?$/, '$1');
-  return maskedCPF;
-}
